62ad28dce5f9bf2f83e11b397b7a66573c35fcf1
Jorge_JJG <48150256+Jo19r85ge@users.noreply.github.com>
2019-12-04 08:31:52 +0100

    New example

---
+++ Ejercicio2 Matriz/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Ejercicio2_Matriz
+{
+    class Program
+    {
+        class Crear
+        {
+            public static int[,] Matriz(int filas, int columnas)
+            {
+                var myMatriz = new int[filas, columnas];
+
+                for (var i = 0; i < myMatriz.GetLength(0); i++)
+                {
+                    for (var j = 0; j < myMatriz.GetLength(1); j++)
+                    {
+                        myMatriz[i, j] = 1;
+                        Console.Write(" || {0} || ", myMatriz[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+
+                return myMatriz;
+            }
+        }
+
+        class Eliminar
+        {
+            public static int[,] Posicion(int fila, int columna, int[,] myMatriz)
+            {
+                myMatriz[fila - 0, columna - 0] = 0;
+
+                for (var i = 0; i < myMatriz.GetLength(0); i++)
+                {
+                    for (var j = 0; j < myMatriz.GetLength(1); j++)
+                    {
+                        Console.Write(" || {0} || ", myMatriz[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+
+                return myMatriz;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos a crear la matriz");
+
+            Console.Write("Numero de filas: ");
+            var filas = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Numero de columnas: ");
+            var columnas = Int32.Parse(Console.ReadLine());
+
+            var crearMatriz = Crear.Matriz(filas, columnas);
+
+            Console.Write("Que fila quieres eliminar un valor: ");
+            var filaIndice = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Que columna quieres eliminar un valor: ");
+            var columnaIndice = Int32.Parse(Console.ReadLine());
+
+            Eliminar.Posicion(filaIndice, columnaIndice, crearMatriz);
+        }
+    }
+}
+
+
+
+   
